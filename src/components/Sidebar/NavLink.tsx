a7f0d957eb90ad0b89597ab4e3c1dae6c7e47602
gustavosilvaf <gustavofsilva@email.com>
2021-10-22 16:20:31 -0300

    Created active link on sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,22 +1,27 @@
-import { Link as ChakraLink, Icon, Text, LinkProps } from "@chakra-ui/react";
-import { IconType } from "react-icons";
+import {Icon, Link as ChakraLink, LinkProps, Text} from "@chakra-ui/react";
+import {IconType} from "react-icons";
 import Link from 'next/link'
+import {useRouter} from "next/dist/client/router";
 
 export interface NavLinkProps extends LinkProps {
-  name: string;
-  icon: IconType;
-  link: string;
+    name: string;
+    icon: IconType;
+    link: string;
 }
 
-export const NavLink = ({ name, icon, link, ...rest }: NavLinkProps) => {
-  return (
-      <Link href={link} passHref>
-          <ChakraLink display="flex" align="center" {...rest}>
-              <Icon as={icon} fontSize="20" />
-              <Text ml="4" fontWeight="medium">
-                  {name}
-              </Text>
-          </ChakraLink>
-      </Link>
-  );
+export const NavLink = ({name, icon, link, ...rest}: NavLinkProps) => {
+    const {asPath} = useRouter();
+
+    const colorToUse = asPath === link || asPath.startsWith(link)? "pink.500" : "gray.50";
+
+    return (
+        <Link href={link} passHref>
+            <ChakraLink display="flex" align="center" {...rest} color={colorToUse}>
+                <Icon as={icon} fontSize="20"/>
+                <Text ml="4" fontWeight="medium">
+                    {name}
+                </Text>
+            </ChakraLink>
+        </Link>
+    );
 };
