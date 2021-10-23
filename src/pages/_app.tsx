82091c612ce3953267ebc4e15b30acae2af0848b
gustavosilvaf <gustavofsilva@email.com>
2021-10-23 15:18:46 -0300

    Added react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SideBarContextProvider } from "../context/SidebarContext";
 import { makeServer } from "../services/mirage";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 if (process.env.NODE_ENV === "development") {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SideBarContextProvider>
-        <Component {...pageProps} />
-      </SideBarContextProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SideBarContextProvider>
+          <Component {...pageProps} />
+        </SideBarContextProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
